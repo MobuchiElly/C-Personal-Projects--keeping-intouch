08cdd93f389a8adda307874a0988af160af7e080
MobuchiElly <ugwueleazer@gmail.com>
2023-04-02 21:51:22 +0100

    Additions to my code

---
+++ malloc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int b;
+    int *p ;
+    *p = malloc(sizeof(int));
+
+
+}
---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("%d", printf("%s\t","Hello World!"));
+    printf("\n");
+    printf("%d", printf("%d\t", 12));
+
+}
